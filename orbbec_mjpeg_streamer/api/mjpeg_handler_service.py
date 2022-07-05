25120acbc7dba1eeee19a38683f5b622c65b33c2
DeltamanZ <true.deltaman@yandex.ru>
2022-07-05 15:31:27 +0300

    Реализация вывода потока

--- orbbec_mjpeg_streamer/api/mjpeg_handler_service.py
+++ orbbec_mjpeg_streamer/api/mjpeg_handler_service.py
@@ -1,15 +1,22 @@
-import logging
-
+from aiohttp import web, MultipartWriter
 from aiohttp_cors import CorsViewMixin
+from asyncore import write
 from m7_aiohttp.util.logged import logged
+import logging
 
 
 logger = logging.getLogger('orbbec-mjpeg-streamer')
 
 
 class MjpegHandlerService(CorsViewMixin):
+    def __init__(self, app) -> None:
+        super().__init__()
+        self.app = app
 
     @logged(logger)
     async def mjpeg_handler_rgb(self, request):
-        # TODO: реализуем метод, генерирующий mjpeg-поток на основе кадров из переменной app['frame']
-        pass
+        response = web.StreamResponse()
+        response.content_type = 'multipart/x-mixed-replace; boundary=frame'
+        await response.prepare(request)
+        while True:
+            await response.write(self.app["frame"])
