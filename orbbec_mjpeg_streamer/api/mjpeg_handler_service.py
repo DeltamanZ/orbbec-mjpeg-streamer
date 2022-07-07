cf46853ff465ad068dadbaf1585116cc80bc0cbb
DeltamanZ <true.deltaman@yandex.ru>
2022-07-07 12:56:39 +0300

    endpoint для вывода дистацнии до ближайшей точки

--- orbbec_mjpeg_streamer/api/mjpeg_handler_service.py
+++ orbbec_mjpeg_streamer/api/mjpeg_handler_service.py
@@ -1,8 +1,10 @@
+from math import dist
 from aiohttp import web, MultipartWriter
 from aiohttp_cors import CorsViewMixin
 from asyncore import write
 from m7_aiohttp.util.logged import logged
 import logging
+import asyncio
 
 
 logger = logging.getLogger('orbbec-mjpeg-streamer')
@@ -28,4 +30,16 @@ class MjpegHandlerService(CorsViewMixin):
         while True:
             await response.write(request.app["depth"])
 
-    
+    @logged(logger)
+    async def mjpeg_handler_min_distance(self, request):
+        resp = web.StreamResponse(status=200, 
+                              reason='OK', 
+                              headers={'Content-Type': 'text/html'})
+        await resp.prepare(request)
+        await resp.write(f"<html><body id=\"test\">Test</body></html>".encode("cp1251"))
+        while True:
+            dist = request.app["min_distance"]
+            await resp.write(f"<script>if (test === undefined) var test = document.getElementById(\"test\"); test.innerText = \"{dist}\" </script>".encode("utf-8"))
+            await resp.drain()
+            await asyncio.sleep(1)
+        # return web.Response(text=request.app["min_distance"])  
