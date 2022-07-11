ce18e44a20e8ecfa1a79ef6d7a1ef3326c06f901
DeltamanZ <true.deltaman@yandex.ru>
2022-07-11 14:10:51 +0300

    реализация двух методов

--- orbbec_mjpeg_streamer/api/rpc.py
+++ orbbec_mjpeg_streamer/api/rpc.py
@@ -1,9 +1,21 @@
+import asyncio
+from importlib.metadata import metadata
+from time import time
+from aiohttp import request
 from aiohttp_jsonrpc import handler
+from orbbec_mjpeg_streamer.database.Database import add_frame, get_frames_count
 
 
 class RPC(handler.JSONRPCView):
-    def rpc_saveFace(self):
-        pass
+    async def rpc_saveFace(self):
+        start_time = time()
+        while time() - start_time < 10:
+            if self.request.app["face_found"] == True:
+                frame = self.request.app["last_frame"]
+                await add_frame(frame)
+                return "Picture has been taken"
+            await asyncio.sleep(0.1)
+        return "Timeout"
     
-    def rpc_getFacesCount(self):
-        pass
+    async def rpc_getFacesCount(self):
+        return await get_frames_count()
