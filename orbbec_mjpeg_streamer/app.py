34316fde104d134c7399df8ec8e944b438b7197e
DeltamanZ <true.deltaman@yandex.ru>
2022-07-11 14:11:17 +0300

    endpoint

--- orbbec_mjpeg_streamer/app.py
+++ orbbec_mjpeg_streamer/app.py
@@ -12,6 +12,7 @@ from aiohttp import web
 
 from orbbec_mjpeg_streamer.api.mjpeg_handler_service import MjpegHandlerService
 from orbbec_mjpeg_streamer.scanner.scanner import Scanner
+from .api.rpc import RPC
 
 logger = logging.getLogger('app')
 
@@ -90,9 +91,7 @@ def create_app(loop: AbstractEventLoop = None, config: dict = None) -> web.Appli
     app['scanner'] = scanner
     app['camera_service'] = MjpegHandlerService()
 
-    cors.add(app.router.add_route('GET', '/', app['camera_service'].mjpeg_handler_rgb))  # endpoint, на котором мы можем посмотреть mjpeg-поток. Пример http://192.168.1.245:8080/
-    # cors.add(app.router.add_route("GET", "/depth", app["camera_service"].mjpeg_handler_depth)) # endpoint for depth sensor stream
-    # cors.add(app.router.add_route("GET", "/distance", app["camera_service"].mjpeg_handler_min_distance)) # endpoint for depth sensor stream
+    cors.add(app.router.add_route('POST', '/', RPC))
 
     app.on_startup.append(on_app_start)
     app.on_shutdown.append(on_app_stop)
